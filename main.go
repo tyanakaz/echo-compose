f967df111580151bf10d439841a1d76141e345a5
tyanakaz <xe314m@gmail.com>
2019-03-26 16:21:17 +0900

    Fix main.go

--- main.go
+++ main.go
@@ -14,10 +14,10 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Route => handler
+  // Route => handler
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!\n")
-	})
+  })
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
